33c8a3ccd598a1f4deaa26f4b5a6fbd6bf7d47da
Mehran Hydary <mehran.hydary@gmail.com>
2019-09-04 16:55:49 -0400

    changed formating

--- routes/api/consolidatedPriceEntries.js
+++ routes/api/consolidatedPriceEntries.js
@@ -4,7 +4,7 @@ const ConsolidatedPriceEntry = require("../../models/ConsolidatedPriceEntry");
 
 // Should get all of the price entries that are in the db
 router.get('/', (req, res) => {
-    ConsolidatedPriceEntry.find({}).sort({$natural:1}).exec((err, consolidatedPriceEntries) => {
+    ConsolidatedPriceEntry.find({}).sort({$natural:-1}).exec((err, consolidatedPriceEntries) => {
         if(err) 
             return res.status(400).json({err: 'Error getting consolidated price entries from db: ' + err});
         return res.status(200).json({ consolidatedPriceEntries })
